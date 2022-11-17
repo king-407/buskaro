3c282cbb6b59cc3b73ab1ecf0a5eaa9fbe575490
Vedasto <prajesh.eleven118@gmail.com>
2022-11-17 17:32:01 +0530

    - setup navigation

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BusBoardingScreen from './screens/BusBoardingScreen';
+import BusInfoScreen from './screens/BusInfoScreen';
+import BusListScreen from './screens/BusListScreen';
+import HomeScreen from './screens/HomeScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import BusJourneyScreen from './screens/BusJourneyScreen';
+import AlertScreen from './screens/AlertScreen';
+
+const Stack = createNativeStackNavigator()
+
 export default function App() {
   return (
     <NavigationContainer>
-      {/*  */}
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="BusList" component={BusListScreen} />
+        <Stack.Screen name="BusBoarding" component={BusBoardingScreen} />
+        <Stack.Screen name="BusInfo" component={BusInfoScreen} />
+        <Stack.Screen name="Payment" component={PaymentScreen} />
+        <Stack.Screen name="BusJourney" component={BusJourneyScreen} />
+        <Stack.Screen name="Alert" component={AlertScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
