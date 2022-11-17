ddd058d1a26aa19d8d95c47f7bec9ec53be9a017
Vedasto <prajesh.eleven118@gmail.com>
2022-11-17 17:04:10 +0530

    added bus journey screen

---
+++ screens/BusJourneyScreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class BusJourneyScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text>BusJourneyScreen</Text>
+            </View>
+        )
+    }
+}
+
+export default BusJourneyScreen
